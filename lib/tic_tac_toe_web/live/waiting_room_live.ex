e89c54c8595ecb3c5f912b70fc7ec9409793eba0
Tom Reis <tom@commercekitchen.com>
2019-09-05 10:41:42 -0600

    Initiate game request cycle by clicking score

--- lib/tic_tac_toe_web/live/waiting_room_live.ex
+++ lib/tic_tac_toe_web/live/waiting_room_live.ex
@@ -10,24 +10,30 @@ defmodule TicTacToeWeb.WaitingRoomLive do
     Presence.track(
       self(),
       "players",
-      username,
-      %{wants_to_play_id: nil}
+      user_id,
+      %{name: username, high_score: :rand.uniform(1000), wants_to_play_id: nil}
     )
 
     TicTacToeWeb.Endpoint.subscribe("players")
 
     {:ok,
      assign(socket,
-       username: username,
-       players: list_players(username)
+       user_id: user_id,
+       players: list_players(user_id)
      )}
   end
 
   def handle_info(
         %{event: "presence_diff", payload: state},
-        socket = %{assigns: %{username: username}}
+        socket = %{assigns: %{user_id: user_id}}
       ) do
-    {:noreply, assign(socket, username: username, players: list_players(username))}
+    {:noreply, assign(socket, user_id: user_id, players: list_players(user_id))}
+  end
+
+  def handle_event("request-game", %{"opponent" => opponent_id}, socket) do
+    # TODO: update presence with wants_to_play_id
+    # TODO: figure out player accept back and forth
+    {:noreply, socket}
   end
 
   # TODO: move the list_players logic to `tic_tac_toe` context?
@@ -37,18 +43,18 @@ defmodule TicTacToeWeb.WaitingRoomLive do
     |> get_player_info()
   end
 
-  defp list_players(username = username) do
+  defp list_players(user_id = user_id) do
     Presence.list("players")
-    |> Enum.reject(fn {player_name, _metas} ->
-      player_name == username
+    |> Enum.reject(fn {player_id, _metas} ->
+      player_id == user_id
     end)
     |> get_player_info
   end
 
   defp get_player_info(presence_list) do
     presence_list
-    |> Enum.map(fn {username, metas} ->
-      username
+    |> Enum.map(fn {user_id, data} ->
+      %{user_id: user_id, data: data[:metas] |> List.first()}
     end)
   end
 end
